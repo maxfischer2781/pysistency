7b61f1849c5377615fd7a2fc4008ca98d70b51c7
Max Fischer <maxfischer2781@gmail.com>
2016-07-28 22:43:17 +0200

    cleanup

--- pysistency/utilities/std_clone.py
+++ pysistency/utilities/std_clone.py
@@ -21,19 +21,18 @@ def inherit_docstrings(*, inherit_from):
             attr_doc = getattr(attr, '__doc__', None)
             # clone docstring
             if attr_doc is None:
-                if hasattr(inherit_from, name):
-                    try:
-                        setattr(attr, '__doc__', getattr(
-                            getattr(inherit_from, name),
-                            '__doc__'
-                        ))
-                    except AttributeError:
-                        pass
+                try:
+                    setattr(attr, '__doc__', getattr(
+                        getattr(inherit_from, name),
+                        '__doc__'
+                    ))
+                except AttributeError:
+                    pass
             # insert docstring - be pedantic as it's an explicit request
             elif ':__doc__:' in attr_doc:
                 if not hasattr(inherit_from, name):
                     raise ValueError('Cannot inherit docstring %s.%s: No source attribute' % (cls.__qualname__, name))
-                if not hasattr(getattr(inherit_from, name), '__doc__'):
+                if getattr(getattr(inherit_from, name), '__doc__', None) is None:
                     raise ValueError('Cannot inherit docstring %s.%s: No source docstring' % (cls.__qualname__, name))
                 # setattr will raise by itself if __doc__ cannot be changed
                 setattr(
