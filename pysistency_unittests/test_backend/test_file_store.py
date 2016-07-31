8bf556d3d54a163ef87448459106108576b6a7b3
Max Fischer <maxfischer2781@gmail.com>
2016-07-31 13:14:59 +0200

    file store tests consistently use temporary directory

--- pysistency_unittests/test_backend/test_file_store.py
+++ pysistency_unittests/test_backend/test_file_store.py
@@ -1,3 +1,4 @@
+import os
 import unittest
 import tempfile
 
@@ -5,19 +6,25 @@ from pysistency.backend import file_store
 
 
 class TestFileBucketStore(unittest.TestCase):
+    def setUp(self):
+        self.temp_dir = tempfile.TemporaryDirectory()
+        self.store_uri = 'file://' + os.path.abspath(self.temp_dir.name).strip('/') + '/'
+
+    def tearDown(self):
+        self.temp_dir.cleanup()
+
     def test_store_uri(self):
         # recognized parameter
         self.assertEqual(
-            file_store.FileBucketStore(store_uri='file:///bar/foo;pickleprotocol=0')._pickle_protocol,
+            file_store.FileBucketStore(store_uri=self.store_uri + ';pickleprotocol=0')._pickle_protocol,
             0
         )
         # unrecognized parameter
         with self.assertRaises(ValueError):
-            file_store.FileBucketStore(store_uri='file://bar;barfoo=0')
+            file_store.FileBucketStore(store_uri=self.store_uri + ';barfoo=0')
 
     def test_bucket_store(self):
-        temp_dir = tempfile.TemporaryDirectory()
-        bucket_store = file_store.FileBucketStore(store_uri='file:/' + temp_dir.name)
+        bucket_store = file_store.FileBucketStore(store_uri=self.store_uri)
         # head just uses buckets, sufficent to test both
         with self.assertRaises(file_store.base_store.BucketNotFound):
             bucket_store.fetch_head()
@@ -26,4 +33,3 @@ class TestFileBucketStore(unittest.TestCase):
         bucket_store.free_head()
         with self.assertRaises(file_store.base_store.BucketNotFound):
             bucket_store.free_head()
-        temp_dir.cleanup()
